b52329bb1a1d5af2b8c23f1f2def81260d6c06b1
Byrdann Fox <mobileupstate@gmail.com>
2015-03-05 20:35:30 -0500

    Updates...

--- AngularJS/js/controllers/core.controller.js
+++ AngularJS/js/controllers/core.controller.js
@@ -1,5 +1,27 @@
-function coreCtrl($scope) {
-	$scope.name = {
-		text: 'username'
-	};
-}
+// DI...
+var coreApp = angular.module('CoreApp', ['ngRoute']);
+
+coreApp.config(function ($routeProvider) {
+
+  $routeProvider
+    .when('/',
+      {
+        controller: 'CoreController',
+        templateUrl: 'partials/view.one.html'
+      })
+    .when('/partial2',
+      {
+        controller: 'CoreController',
+        templateUrl: 'partials/view.two.html'
+      })
+    .otherwise({ redirectTo: '/' });
+
+});
+
+coreApp.controller('CoreController', function($scope) {
+
+  $scope.items = [{name: 'Ryan Poplin', city: 'Greenville, SC'}, 
+                  {name: 'Jen Tucker', city: 'Charlotte, NC'}, 
+                  {name: 'Jen Scotts', city: 'Atlanta, GA'}];
+
+});
